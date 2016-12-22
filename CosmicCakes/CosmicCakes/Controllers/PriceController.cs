29af67e77be1e690d142e95310a618b8a0db1b8e
OilRig <igorguetta@mail.ru>
2016-12-22 15:16:35 +0300

    Adding views

---
+++ CosmicCakes/CosmicCakes/Controllers/PriceController.cs
@@ -0,0 +1,15 @@
+﻿using CosmicCakes.Models;
+using System.Web.Mvc;
+
+namespace CosmicCakes.Controllers
+{
+    public class PriceController : Controller
+    {
+        // GET: Price
+        public ActionResult Index()
+        {
+            var priceModel = new CreateCakeModel();
+            return View(priceModel);
+        }
+    }
+}
---
+++ CosmicCakes/CosmicCakes/Models/CreateCakeModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CosmicCakes.Models
+{
+    public class CreateCakeModel
+    {
+        public IList<string> Bisquit { get; set; }
+        public IList<string> Berries { get; set; }
+        public IList<string> Filling { get; set; }
+        public IList<string> Decorations { get; set; }
+
+        public CreateCakeModel()
+        {
+            Bisquit = new List<string>
+            {
+                "Шоколадный","Лимонный"
+            };
+            Berries = new List<string>()
+            {
+                "Клубника","Малина","Ежевика","Голубика"
+            };
+            Filling = new List<string>
+            {
+                "Карамель","Желе","Курд"
+            }
+            ;
+            Decorations = new List<string>
+            {
+                "Горка красных ягод","Горка черных ягод","Кумкваты","Инжир","Цветы"
+            };
+        }
+    }
+}
