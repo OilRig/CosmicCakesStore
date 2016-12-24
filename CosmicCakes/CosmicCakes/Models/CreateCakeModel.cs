c0629214bc053b98cd5bb702abb4b96a3f7aded2
OilRig <igorguetta@mail.ru>
2016-12-24 12:26:29 +0300

    added price page

--- CosmicCakes/CosmicCakes/Models/CreateCakeModel.cs
+++ CosmicCakes/CosmicCakes/Models/CreateCakeModel.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CosmicCakes.Models
 {
     public class CreateCakeModel
     {
+        [Required]
+        public double Weight { get; set; }
         public IList<string> Bisquit { get; set; }
         public IList<string> Berries { get; set; }
         public IList<string> Filling { get; set; }
         public IList<string> Decorations { get; set; }
+        public IList<string> Cream { get; set; }
 
         public CreateCakeModel()
         {
             Bisquit = new List<string>
             {
-                "Шоколадный","Лимонный"
+                "Шоколадный", "Ванильный", "Лимонный", "Мандариновый", "Миндальный",
+                "Лавандовый", "Морковный", "Красный бархат", "Чизкейк", "Лайм", "Черничный"
             };
             Berries = new List<string>()
             {
@@ -21,12 +26,22 @@ namespace CosmicCakes.Models
             };
             Filling = new List<string>
             {
-                "Карамель","Желе","Курд"
+                 "Фруктовое желе на со свежими ягодами",
+                "Цитрусовый курд (лимон, апельсин, мандарин)",
+                "Домашний шоколадный мусс",
+                "Орехи: миндаль, фундук, фисташка, грецкий, кешью",
+                "Домашняя карамель с пряными нотками корицы, кардамона, имбиря и гвоздики",
+                "Сезонные ягоды и фрукты",
+                "Джем, варенье"
             }
             ;
             Decorations = new List<string>
             {
-                "Горка красных ягод","Горка черных ягод","Кумкваты","Инжир","Цветы"
+                "Горка красных ягод","Горка черных ягод","Цветы","Другое(укажите в дополнении)"
+            };
+            Cream = new List<string>
+            {
+                "Сливочно-творожный"," Шоколадный", "Крем на белом швейцарском шоколаде"
             };
         }
     }
---
+++ CosmicCakes/CosmicCakes/Scripts/BerryChecker.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("ul.dropdown-menu input[type=checkbox]").each(function () {
+        $(this).change(function () {
+            var line = "";
+            $("ul.dropdown-menu input[type=checkbox]").each(function () {
+                if ($(this).is(":checked")) {
+                    line += $("+ span", this).text() + ";";
+                }
+            });
+            $("input.form-control").val(line);
+        });
+    });
+});
