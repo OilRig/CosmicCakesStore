83d06afbd52d7222deb521e1b1cb6bdf9e8d8b6f
iborzenko <igorguetta@mail.ru>
2017-03-10 19:11:56 +0300

    -клиентская валидация формы заказа

--- CosmicCakes/CosmicCakes/App_Start/BundleConfig.cs
+++ CosmicCakes/CosmicCakes/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace CosmicCakesWebApp
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui.js"));
+                        "~/Scripts/jquery-ui.js",
+                        "~/Scripts/jquery.validate.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ CosmicCakes/CosmicCakes/Controllers/DebugController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CosmicCakes.Models;
+
+namespace CosmicCakes.Controllers
+{
+    public class DebugController : Controller
+    {
+        // GET: Debug
+        public ActionResult Index()
+        {
+            return View(new OrderModel());
+        }
+    }
+}
