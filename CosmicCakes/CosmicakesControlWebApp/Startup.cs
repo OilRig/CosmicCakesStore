b36c260416bd1605a211a86b07539e31b692b6aa
OilRig <igorguetta@mail.ru>
2017-03-16 23:33:42 +0300

    -проект управления

---
+++ CosmicCakes/CosmicakesControlWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CosmicakesControlWebApp.Startup))]
+namespace CosmicakesControlWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
