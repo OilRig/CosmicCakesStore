2cc5a2dddd2214a3f81c204e2e489da36ef6fca2
OilRig <igorguetta@mail.ru>
2017-03-28 00:05:07 +0300

    -done feddbacks

--- CosmicCakes/CosmicCakes/Scripts/feedbackValidator.js
+++ CosmicCakes/CosmicCakes/Scripts/feedbackValidator.js
@@ -1,9 +1,18 @@
 ﻿(function ($) {
     $(document).ready(function () {
+
+        $.validator.addMethod("emailRegx", function (value, element, regexpr) {
+            return regexpr.test(value);
+        }, "<p style='color:red;'>Ваш email не похож на example@domain.ru</p>");
+
         $("#feedbackForm").validate({
             rules: {
                 Author: "required",
-                Content: "required"
+                Content: "required",
+                Email: {
+                    emailRegx: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$ |^$/,
+                    required:false
+                }
             },
             messages: {
                 Author: "<p style='color:red;'>А как Вас зовут?</p>",
