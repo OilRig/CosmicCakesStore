035f0d0fc4f8fb110cc2f28b0cec0a065b7b8aa8
iborzenko <igorguetta@mail.ru>
2017-03-29 18:15:11 +0300

    -отзывы сделаны

--- CosmicCakes/CosmicCakes/App_Start/BundleConfig.cs
+++ CosmicCakes/CosmicCakes/App_Start/BundleConfig.cs
@@ -28,9 +28,7 @@ namespace CosmicCakesWebApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/jquery-ui.css",
-                      "~/Content/jquery-ui.structure.css",
-                      "~/Content/jquery-ui.theme.css"));
+                      "~/Content/jquery-ui.min.css"));
         }
     }
 }
