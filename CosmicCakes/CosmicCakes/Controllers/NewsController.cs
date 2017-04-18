d24bbae9aa09dff890b1585e3eeab6ce80f3a7f9
iborzenko <igorguetta@mail.ru>
2017-04-18 18:51:47 +0300

    -подписка

---
+++ CosmicCakes/CosmicCakes/Controllers/NewsController.cs
@@ -0,0 +1,24 @@
+﻿using CosmicCakes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosmicCakes.Controllers
+{
+    public class NewsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubscribeForNews(NewsSubscribeModel model)
+        {
+            return View("SubscribeCompleted");
+        }
+    }
+}
---
+++ CosmicCakes/CosmicCakes/Scripts/SubscribeValidator.js
@@ -0,0 +1,21 @@
+﻿ (function ($) {
+
+            $.validator.addMethod("emailRegx", function (value, element, regexpr) {
+                return regexpr.test(value);
+            }, "<p style='color:red;'>Ваш email не похож на example@domain.ru</p>");
+
+            $("#subscribeForm").validate({
+                rules: {
+                    Email: {
+                        emailRegx: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$|^$/,
+                        required: true
+                    }
+                },
+                messages: {
+                    Email: {
+                        required: "<p style='color:red;'>А куда же нам отправлять письма?</p>"
+                    }
+                }
+            });
+        })
+        (jQuery);
