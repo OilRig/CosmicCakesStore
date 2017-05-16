71b8369ce8f2efdc50e0f9240a5f381fafbbbbcd
OilRig <igorguetta@mail.ru>
2017-05-16 07:58:07 +0300

    -скрипт валидации подлатал

--- CosmicCakes/CosmicCakes/Scripts/orderValidator.js
+++ CosmicCakes/CosmicCakes/Scripts/orderValidator.js
@@ -7,7 +7,7 @@
                    {
                        required:true
                    },
-                CakeWeight: {
+                CakeStringWeight: {
                     required:true,
                     min: 1.5,
                     max:20,
@@ -21,7 +21,7 @@
             messages: {
                 ExpireDateString: "<p style='color:red;'>Кажется,Вы не указали дату для тортика</p>",
                 CustomerName: "<p style='color:red;'>Как же Вас зовут?</p>",
-                CakeWeight: {
+                CakeStringWeight: {
                     required: "<p style='color:red;'>Не указан вес тортика</p>",
                     min: "<p style='color:red;'>Минимальный вес - 1.5 кг</p>",
                     max: "<p style='color:red;'>Больше 20 кг не сможем поместить в холодильник :(</p>"
