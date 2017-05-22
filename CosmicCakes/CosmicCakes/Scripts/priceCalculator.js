e8502674326c262da0eb31aedd3eaffeb33b20f5
OilRig <igorguetta@mail.ru>
2017-05-22 23:48:07 +0300

    -калькулятор цены

--- CosmicCakes/CosmicCakes/Scripts/priceCalculator.js
+++ CosmicCakes/CosmicCakes/Scripts/priceCalculator.js
@@ -1,11 +1,13 @@
 ﻿(function ($) {
     $('input#CakeStringWeight').on('input', function () {
         var value = $(this).val();
+        if (value.includes(","))
+            value = value.replace(",", ".");
         var kgPrice = $('div#cakeName').data('kgprice');
-        var price = value * kgPrice;
+        var price = Math.round((value * kgPrice)*100)/100;
         if(!isNaN(price))
             $('#priceHolder').text(price+"р.");
         else
-            $('#priceHolder').text('Напишите цену через запятую');
+            $('#priceHolder').text('Какой-то вес странный...');
     });
 })(jQuery)
