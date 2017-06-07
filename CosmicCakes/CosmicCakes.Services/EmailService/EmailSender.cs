2300e1274f84fc75ca87b872ffdd000fa667dfaa
Igor Borzenko <igorguetta@mail.ru>
2017-06-07 12:46:19 +0300

    Update EmailSender.cs

--- CosmicCakes/CosmicCakes.Services/EmailService/EmailSender.cs
+++ CosmicCakes/CosmicCakes.Services/EmailService/EmailSender.cs
@@ -18,7 +18,7 @@ namespace CosmicCakes.Services.EmailService
                 m.Body = message;
 
                 var smtp = new SmtpClient("smtp.gmail.com", 587);
-                smtp.Credentials = new NetworkCredential("cosmicakesofficial@gmail.com", "gbczgjgf2345");
+                smtp.Credentials = new NetworkCredential("xxxxxxxxxx", "xxxxxxxxxxxxx");
                 smtp.EnableSsl = true;
                 smtp.Send(m);
             }
