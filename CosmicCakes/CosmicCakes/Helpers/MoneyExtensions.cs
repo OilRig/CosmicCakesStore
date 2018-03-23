63eaa08b1ddfeb5b5bef1b005402264f76ab07bc
iborzenko <iborzenko@baltbet.ru>
2018-03-23 20:29:59 +0300

    []money decimal

---
+++ CosmicCakes/CosmicCakes/Helpers/MoneyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace CosmicCakes.Helpers
+{
+    public static class MoneyExtensions
+    {
+        public static string ToMoneyString(this decimal amount, CultureInfo culture)
+        {
+            return string.Format(culture, "{0:0}", amount);
+        }
+    }
+}
